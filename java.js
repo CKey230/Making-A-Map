caa5378ed68cbab16badda2fe84ae94cd5829d16
Ckey230 <Ckey230@gmail.com>
2022-10-30 10:31:28 -0400

    browser foundation

--- java.js
+++ java.js
@@ -1,51 +1,43 @@
-//Leaflet Map
-  const map = L.map('map').setView([0,0], 3);
 
-  //Create Tiles
+
+
+
+
+
+
+//Geographic Location
+function userLocation(){
+  //leaflet map
+  const map = L.map('map').setView([0,0], 15);
+//create tiles 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-
-//Geographic Location
-  function userLocation(){
-
-      if(!navigator.geolocation){
-        console.log('not supported by this browser');
-      
-      }
-      else {
-        console.log('supported by this browser')
-        navigator.geolocation.getCurrentPosition(position => {
-          const pos = {
-            lat: position.coords.latitude,
-            long: position.coords.longitude,
-          }
-          L.marker([pos.lat,pos.long]).addTo(map) .bindPopup('You are here').openPopup();
-          console.log(pos)
-        });
-      }
-    }
-      userLocation();
-     
-//misc.. on click map popup 
-    var popup = L.popup();
+    if(!navigator.geolocation){
+      console.log('not supported by this browser');
     
-
-    function onMapClick(e) {
-        popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
     }
-    
-    map.on('click', onMapClick);
+    else {
+      console.log('supported by this browser')
+      navigator.geolocation.getCurrentPosition(position => {
+        const pos = {
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        }
+        L.marker([pos.lat,pos.long]).addTo(map) .bindPopup('You are here').openPopup();
+        console.log(pos)
+      });
+    }
+   
+  }
+    userLocation();
 
+    
 
     const sumbit = document.getElementById("submit");
     sumbit.addEventListener("click", function(e){
       document.getElementById('business').value
      console.log(business)
-
-    });
-   
+    })
+    
